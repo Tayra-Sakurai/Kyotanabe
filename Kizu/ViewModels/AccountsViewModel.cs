fba6b624dfe7573087ec4cb1ffbad31154a0859b
SAKURAI Tayra <taira_sakurai@outlook.jp>
2026-04-29 17:48:46 +0900

    Added a file.

---
+++ Kizu/ViewModels/AccountsViewModel.cs
@@ -0,0 +1,47 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Kizu.Contexts;
+using Kizu.Models;
+using Kizu.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kizu.ViewModels
+{
+    public partial class AccountsViewModel : ObservableObject
+    {
+        private readonly IDatabaseService<KizuContext> databaseService;
+
+        [ObservableProperty]
+        private ObservableCollection<Account> accounts;
+
+        public AccountsViewModel(IDatabaseService<KizuContext> databaseService)
+        {
+            this.databaseService = databaseService;
+
+            Accounts = [];
+        }
+
+        [RelayCommand(AllowConcurrentExecutions = false)]
+        public async Task LoadAsync()
+        {
+            Accounts.Clear();
+
+            foreach (var account in await databaseService.GetEntitiesAsync(context => context.Accounts))
+                Accounts.Add(account);
+        }
+
+        [RelayCommand(AllowConcurrentExecutions = false)]
+        public async Task AddAsync()
+        {
+            Account account = new();
+            await databaseService.AddAsync(account);
+
+            await LoadAsync();
+        }
+    }
+}
