c7aafd3c26a85949f9d4a8e07ebe6bb3e9fc53c8
SAKURAI Tayra <taira_sakurai@outlook.jp>
2026-05-02 20:03:57 +0900

    Updated some.

---
+++ Kyotanabe/Kyotanabe/Templates/ValidationNumberBox.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Documents;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Kyotanabe.Templates
+{
+    [TemplatePart(Name = "Value", Type = typeof(NumberBox))]
+    [TemplatePart(Name = "Message", Type = typeof(TextBlock))]
+    public sealed partial class ValidationNumberBox : Control
+    {
+        private NumberBox? numberBox;
+        private TextBlock? itemsRepeater;
+
+        private INotifyDataErrorInfo? oldDataContext;
+
+        public ValidationNumberBox()
+        {
+            DefaultStyleKey = typeof(ValidationNumberBox);
+
+            DataContextChanged += ValidationNumberBox_DataContextChanged;
+        }
+
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            numberBox = (NumberBox)GetTemplateChild("Value");
+            itemsRepeater = (TextBlock)GetTemplateChild("Message");
+
+            numberBox.ValueChanged += NumberBox_ValueChanged;
+
+            RefreshErrors();
+        }
+
+        private void NumberBox_ValueChanged(NumberBox sender, NumberBoxValueChangedEventArgs args)
+        {
+            Value = sender.Value;
+
+            RefreshErrors();
+        }
+
+        private void ValidationNumberBox_DataContextChanged(FrameworkElement sender, DataContextChangedEventArgs args)
+        {
+            if (oldDataContext != null)
+                oldDataContext.ErrorsChanged -= OldDataContext_ErrorsChanged;
+
+            if (DataContext is INotifyDataErrorInfo notifyDataError)
+            {
+                oldDataContext = notifyDataError;
+                oldDataContext.ErrorsChanged += OldDataContext_ErrorsChanged;
+            }
+        }
+
+        private void OldDataContext_ErrorsChanged(object? sender, DataErrorsChangedEventArgs e)
+        {
+            RefreshErrors();
+        }
+
+        public double Value
+        {
+            get => (double)GetValue(ValueProperty);
+            set => SetValue(ValueProperty, value);
+        }
+
+        private static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
+            nameof(Value),
+            typeof(double),
+            typeof(ValidationNumberBox),
+            new(0));
+
+        public string Header
+        {
+            get => (string)GetValue(HeaderProperty);
+            set => SetValue(HeaderProperty, value);
+        }
+
+        private static readonly DependencyProperty HeaderProperty = DependencyProperty.Register(
+            nameof(Header),
+            typeof(string),
+            typeof(ValidationNumberBox),
+            new(default(string)));
+
+        public string PropertyName
+        {
+            get => (string)GetValue(PropertyNameProperty);
+            set => SetValue(PropertyNameProperty, value);
+        }
+
+        private static readonly DependencyProperty PropertyNameProperty = DependencyProperty.Register(
+            nameof(PropertyName),
+            typeof(string),
+            typeof(ValidationNumberBox),
+            new(PropertyNameProperty, OnPropertyNamePropertyChanged));
+
+        private static void OnPropertyNamePropertyChanged(object sender, DependencyPropertyChangedEventArgs args)
+        {
+            if (args.NewValue is not string v || string.IsNullOrWhiteSpace(v))
+                return;
+
+            ((ValidationNumberBox)sender).RefreshErrors();
+        }
+
+        private void RefreshErrors()
+        {
+            if (numberBox is null ||
+                itemsRepeater is null ||
+                DataContext is not INotifyDataErrorInfo dataContext ||
+                string.IsNullOrWhiteSpace(PropertyName))
+                return;
+
+            ValidationResult? result = dataContext.GetErrors(PropertyName).OfType<ValidationResult>().FirstOrDefault(e => e.ErrorMessage is not null);
+            if (result == null)
+            {
+                itemsRepeater.Text = string.Empty;
+                return;
+            }
+
+            if (result.ErrorMessage is not null)
+                itemsRepeater.Text = result.ErrorMessage;
+        }
+    }
+}
---
+++ Kyotanabe/Kyotanabe/Templates/ValidationTextBox.cs
@@ -0,0 +1,162 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Documents;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Kyotanabe.Templates
+{
+    [TemplatePart(Name = "TextBox", Type = typeof(TextBox))]
+    [TemplatePart(Name = "Message", Type = typeof(TextBlock))]
+    public sealed partial class ValidationTextBox : Control
+    {
+        private INotifyDataErrorInfo? oldDataContext;
+
+        private TextBox? textBox;
+        private TextBlock? textBlock;
+
+        public ValidationTextBox()
+        {
+            DefaultStyleKey = typeof(ValidationTextBox);
+
+            DataContextChanged += ValidationTextBox_DataContextChanged;
+        }
+
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            textBox = (TextBox)GetTemplateChild("TextBox");
+            textBlock = (TextBlock)GetTemplateChild("Message");
+
+            textBox.TextChanged += TextBox_TextChanged;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Text = ((TextBox)sender).Text;
+        }
+
+        private void ValidationTextBox_DataContextChanged(FrameworkElement sender, DataContextChangedEventArgs args)
+        {
+            if (oldDataContext != null)
+                oldDataContext.ErrorsChanged -= OldDataContext_ErrorsChanged;
+
+            if (DataContext is INotifyDataErrorInfo notifyDataError)
+            {
+                oldDataContext = notifyDataError;
+                oldDataContext.ErrorsChanged += OldDataContext_ErrorsChanged;
+            }
+
+            RefreshErrors();
+        }
+
+        private void OldDataContext_ErrorsChanged(object? sender, DataErrorsChangedEventArgs e)
+        {
+            RefreshErrors();
+        }
+
+        public string Text {
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
+        private static readonly DependencyProperty TextProperty = DependencyProperty.Register(
+            nameof(Text),
+            typeof(string),
+            typeof(ValidationTextBox),
+            new(default(string)));
+
+        public string Message
+        {
+            get => (string)GetValue(MessageProperty);
+            set => SetValue(MessageProperty, value);
+        }
+
+        private static readonly DependencyProperty MessageProperty = DependencyProperty.Register(
+            nameof(Message),
+            typeof(string),
+            typeof(ValidationTextBox),
+            new(default(string)));
+
+        public string Header
+        {
+            get => (string)GetValue(HeaderProperty);
+            set => SetValue(HeaderProperty, value);
+        }
+
+        private static readonly DependencyProperty HeaderProperty = DependencyProperty.Register(
+            nameof(Header),
+            typeof(string),
+            typeof(ValidationTextBox),
+            new(default(string)));
+
+        public string Property
+        {
+            get => (string)GetValue(PropertyProperty);
+            set => SetValue(PropertyProperty, value);
+        }
+
+        private static readonly DependencyProperty PropertyProperty = DependencyProperty.Register(
+            nameof(Property),
+            typeof(string),
+            typeof(ValidationTextBox),
+            new(PropertyProperty, OnPropertyPropertyChanged));
+
+        private static void OnPropertyPropertyChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (e.NewValue is not string str || string.IsNullOrWhiteSpace(str))
+                return;
+
+            ((ValidationTextBox)sender).RefreshErrors();
+        }
+
+        private void RefreshErrors()
+        {
+            if (Property is not string property ||
+                DataContext is not INotifyDataErrorInfo dataErrorInfo ||
+                textBox is not TextBox tBox ||
+                textBlock is null)
+                return;
+
+            ValidationResult? result = dataErrorInfo.GetErrors(property).OfType<ValidationResult>().FirstOrDefault();
+
+            if (result is null || result == ValidationResult.Success)
+            {
+                Message = string.Empty;
+                Brush? brush = Application.Current.Resources["TextBoxBorderThemeBrush"] as Brush;
+                if (brush != null)
+                    textBox.BorderBrush = brush;
+                return;
+            }
+
+            Message = result.ErrorMessage ?? "An unspecified error occurred.";
+
+            Brush? themeDics = Application.Current.Resources["SystemErrorTextColor"] as Brush;
+            if (themeDics != null)
+                tBox.BorderBrush = themeDics;
+        }
+
+        public string PlaceholderText
+        {
+            get => (string)GetValue(PlaceholderTextProperty);
+            set => SetValue(PlaceholderTextProperty, value);
+        }
+
+        private static readonly DependencyProperty PlaceholderTextProperty = DependencyProperty.Register(
+            nameof(PlaceholderText),
+            typeof(string),
+            typeof(ValidationTextBox),
+            new(default(string)));
+    }
+}
